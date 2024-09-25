f5f529785d7f6c455e86f8dd3ddcece1ee3310c6
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-09-25 23:39:46 +0900

    Title: 서로 다른 BST 개수 세기, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240925/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        dp = new int[n+1];
+        System.out.println(go(n));
+    }
+    static int go(int num){
+        if(num == 0) return dp[0] = 0;
+        if(num == 1) return dp[1] = 1;
+        if(num == 2) return dp[2] = 2;
+        if(num == 3) return dp[3] = 5;
+        if(dp[num] == 0) {
+            for(int i = 1; i < num; ++i){
+                dp[num] += go(num-i) * go(i);
+            }
+        }
+        return dp[num];
+    }
+}
