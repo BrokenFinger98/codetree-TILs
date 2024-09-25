1ceb112f552aa015802bd98bbad5228bb3ec14c1
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-09-25 22:07:55 +0900

    Title: 사각형 채우기 3, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240925/사각형 채우기 3/rectangle-fill-3.java
+++ 240925/사각형 채우기 3/rectangle-fill-3.java
@@ -2,9 +2,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-
+/**
+ *  시간 : 80ms, 메모리 : 11,928KB
+ */
 public class Main {
     static int n;
+    static final int MOD = 1_000_000_007;
     static int[][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,8 +23,8 @@ public class Main {
         dp[2][1] = 7;
         dp[2][0] = 1;
         for(int i = 3; i <= n; ++i){
-            dp[i][0] = dp[i-3][1] + dp[i-1][0];
-            dp[i][1] = dp[i-1][1]*2 + dp[i-2][1] * 3 + dp[i][0] * 2;
+            dp[i][0] = (dp[i-3][1] + dp[i-1][0])%MOD;
+            dp[i][1] = (dp[i-1][1]*2 + dp[i-2][1] * 3 + dp[i][0] * 2)%MOD;
         }
         System.out.println(dp[n][1]);
     }
