7245031287e96ae4676f3d8aeb6fde162cdf3742
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-09-25 23:47:55 +0900

    Title: 서로 다른 BST 개수 세기, Time: 83ms, Memory: 8MB, Status: Passed - CodeTree

--- 240925/서로 다른 BST 개수 세기/number-of-unique-bst.java
+++ 240925/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -15,10 +15,7 @@ public class Main {
         System.out.println(go(n));
     }
     static int go(int num){
-        if(num == 0) return dp[0] = 1;
-        if(num == 1) return dp[1] = 1;
-        if(num == 2) return dp[2] = 2;
-        if(num == 3) return dp[3] = 5;
+        if(num <= 1) return 1;
         if(dp[num] == 0) {
             for(int i = 0; i < num; ++i){
                 dp[num] += go(num-i-1) * go(i);
