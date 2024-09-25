6dbc6d8164f4dbfc4f82a8cc63525473ab1bfee7
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-09-25 23:52:30 +0900

    Title: 사각형 채우기 2, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

--- 240925/사각형 채우기 2/rectangle-fill-2.java
+++ 240925/사각형 채우기 2/rectangle-fill-2.java
@@ -16,7 +16,7 @@ public class Main {
         dp[0] = 1;
         dp[1] = 1;
         for(int i = 2; i <= n; ++i){
-            dp[i] = dp[i-1] + dp[i-2] * 2;
+            dp[i] = (dp[i-1] + dp[i-2] * 2)%MOD;
         }
         System.out.println(dp[n]);
     }
