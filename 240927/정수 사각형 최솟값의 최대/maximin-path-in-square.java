85a13b62290ebd8fe55d4ef7f4108f67b9b3ff0c
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-09-27 21:09:52 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 172ms, Memory: 12MB, Status: Passed - CodeTree

--- 240927/정수 사각형 최솟값의 최대/maximin-path-in-square.java
+++ 240927/정수 사각형 최솟값의 최대/maximin-path-in-square.java
@@ -26,8 +26,8 @@ public class Main {
             dp[i][0] = Math.min(dp[i-1][0], map[i][0]);
             dp[0][i] = Math.min(dp[0][i-1], map[0][i]);
         }
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
                 dp[i][j] = Math.min(Math.max(dp[i-1][j], dp[i][j-1]), map[i][j]);
             }
         }
