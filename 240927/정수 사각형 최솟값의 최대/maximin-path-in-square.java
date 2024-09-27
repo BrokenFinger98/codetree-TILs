c24ac5058aa2d58d33127b37c12430d93acc62e8
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-09-27 21:08:55 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 145ms, Memory: 12MB, Status: Passed - CodeTree

--- 240927/정수 사각형 최솟값의 최대/maximin-path-in-square.java
+++ 240927/정수 사각형 최솟값의 최대/maximin-path-in-square.java
@@ -1,46 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int N;
+    static int n;
     static int[][] map;
     static int[][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        dp = new int[N+1][N+1];
-        map = new int[N+1][N+1];
-        for (int i = 1; i <= N; i++) {
+        n = Integer.parseInt(st.nextToken());
+        map = new int[n+1][n+1];
+        dp = new int[n + 1][n + 1];
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
+            for (int j = 0; j < n; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        for (int i = 1; i <= N; i++) {
-            dp[i][0] = map[i][1];
-            dp[0][i] = map[1][i];
+
+        dp[0][0] = map[0][0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.min(dp[i-1][0], map[i][0]);
+            dp[0][i] = Math.min(dp[0][i-1], map[0][i]);
         }
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                if(i == N && j == N){
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-                }else {
-                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), map[i][j]);
-                }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = Math.min(Math.max(dp[i-1][j], dp[i][j-1]), map[i][j]);
             }
         }
-//        for (int i = 1; i <= N; i++) {
-//            for (int j = 1; j <= N; j++) {
-//                System.out.print(dp[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-        System.out.println(dp[N][N]);
-        br.close();
+        System.out.println(dp[n-1][n-1]);
     }
 }
