5624b1f5405de6a75e67fd1c056fc5e2e9801d29
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-10-08 14:44:48 +0900

    Title: 최장 공통 부분 수열, Time: 79ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 241008/최장 공통 부분 수열/longest-common-sequence.java
+++ 241008/최장 공통 부분 수열/longest-common-sequence.java
@@ -31,7 +31,7 @@ public class Main {
                 if(s1.charAt(i) == s2.charAt(j)){
                     dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-1] + 1);
                 }else{
-                    dp[i][j] = dp[i-1][j];
+                    dp[i][j] = dp[i][j-1];
                 }
             }
         }
