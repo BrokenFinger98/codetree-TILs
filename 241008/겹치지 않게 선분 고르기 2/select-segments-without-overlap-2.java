7770c1db8996384b4c03daeac1c6cc74c9be5d92
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-10-08 09:30:01 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 155ms, Memory: 9MB, Status: Passed - CodeTree

--- 241008/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
+++ 241008/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
@@ -14,30 +14,34 @@ public class Main {
 
         @Override
         public int compareTo(Line o) {
-            if(this.x1 == o.x1) return Integer.compare(this.x2, o.x2);
             return Integer.compare(this.x1, o.x1);
         }
     }
     static int n, end, answer;
     static List<Line> lines = new ArrayList<>();
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         n = Integer.parseInt(br.readLine());
+        dp = new int[n];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             lines.add(new Line(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
         }
         Collections.sort(lines);
-        end = lines.get(0).x2;
-        answer = 1;
-        for (int i = 1; i < n; i++) {
-            Line line = lines.get(i);
-            if(line.x1 > end){
-                end = line.x2;
-                ++answer;
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                int x1I = lines.get(i).x1;
+                int x2J = lines.get(j).x2;
+                if(x2J < x1I)
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
             }
+        }
+        for (int i = 0; i < n; i++) {
+            answer = Math.max(answer, dp[i]);
         }
         System.out.println(answer);
         br.close();
