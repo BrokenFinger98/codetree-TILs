5f2185b8ab163f08792d4ddf3c99f272e9d9d75b
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-10-10 10:05:12 +0900

    Title: 최장 공통 부분 수열 2, Time: 140ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 241010/최장 공통 부분 수열 2/longest-common-sequence-2.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+
+public class Main {
+    static String s1, s2;
+    static int[][] dp;
+    static int last, answer;
+    static ArrayDeque<Character> stack = new ArrayDeque<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        s1 = br.readLine();
+        s2 = br.readLine();
+        dp = new int[s1.length()][s2.length()];
+        dp[0][0] = s1.charAt(0) == s2.charAt(0) ? 1 : 0;
+        for (int i = 1; i < s1.length(); i++) {
+            if(s1.charAt(i) == s2.charAt(0)){
+                dp[i][0] = 1;
+            }else{
+                dp[i][0] = dp[i-1][0];
+            }
+        }
+        for (int i = 1; i < s2.length(); i++) {
+            if(s2.charAt(i) == s1.charAt(0)){
+                dp[0][i] = 1;
+            }else{
+                dp[0][i] = dp[0][i-1];
+            }
+        }
+        for (int i = 1; i < s1.length(); i++) {
+            for (int j = 1; j < s2.length(); j++) {
+                if(s1.charAt(i) == s2.charAt(j)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+                }
+                if(answer < dp[i][j]){
+                    last = i;
+                    answer++;
+                }
+            }
+        }
+        while (answer > 0){
+            stack.push(s1.charAt(last--));
+            answer--;
+        }
+        while (!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+        System.out.println(sb);
+        br.close();
+    }
+}
