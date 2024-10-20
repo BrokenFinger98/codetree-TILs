96ea6268e73eecc661724b16049a957852b2ebef
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-10-20 15:20:58 +0900

    Title: 중복되는 수열, Time: 166ms, Memory: 15MB, Status: Passed - CodeTree

--- 241020/중복되는 수열/duplicate-sequence.java
+++ 241020/중복되는 수열/duplicate-sequence.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
     static class Node{
         Node[] children = new Node[10];
         boolean isEnd;
@@ -14,41 +13,43 @@ public class Main {
             }
         }
     }
-
     static Node root = new Node();
     static int n;
-    static String text, pattern;
+    static String text;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        pattern = br.readLine();
-        for(int i = 0; i < n-1; ++i){
+        for(int i = 0; i < n; ++i){
             text = br.readLine();
-            insertText(text);
+            if(insertText(text) == 0) {
+                System.out.println(0);
+                System.exit(0);
+            }
         }
-        System.out.println(findText(pattern));
+        System.out.println(1);
         br.close();
     }
 
-    static void insertText(String text){
+    static int insertText(String text){
         Node node = root;
         for(int i = 0; i < text.length(); i++){
+            if(node.isEnd) return 0;
             if(node.children[text.charAt(i) - '0'] == null){
                 node.children[text.charAt(i) - '0'] = new Node();
             }
             node = node.children[text.charAt(i) - '0'];
         }
         node.isEnd = true;
+        return 1;
     }
 
     static int findText(String text){
         Node node = root;
         int i;
         for(i = 0; i < text.length(); ++i){
-            if(node.children[text.charAt(i) - '0'] != null){
-                node = node.children[text.charAt(i) - '0'];
-            }else break;
+            if(node.children[text.charAt(i) - '0'] == null) return 1;
+            node = node.children[text.charAt(i) - '0'];
         }
-        return i == text.length() ? 0 : 1;
+        return 0;
     }
 }
